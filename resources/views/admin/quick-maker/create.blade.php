2892bc4a18517ede505f6aba519b3b63535b6649
RichnessInc <omarkhaledjob2001@gmail.com>
2024-07-21 16:45:02 +0300

    push

--- resources/views/admin/quick-maker/create.blade.php
+++ resources/views/admin/quick-maker/create.blade.php
@@ -7,7 +7,8 @@
                 <div class="row">
                     <div class="col-lg">
                         <label for="nameFormControlInput" class="mb-2" class="form-label">{{ __('lang.name') }}</label>
-                        <input type="text" class="form-control" name="name" id="nameFormControlInput" placeholder="{{ __('lang.name') }}">
+                        <input type="text" class="form-control" name="name" id="nameFormControlInput"
+                            placeholder="{{ __('lang.name') }}">
                     </div>
                     <div class="col-12">
                         <br>
@@ -19,16 +20,16 @@
                                     <option value="1">{{ __('lang.yes') }}</option>
                                 </select>
                             </div>
-    
+
                             <div class="col-lg-6">
-                                <label class="form-label" for="form-repeater-1-2">{{ __('lang.module') }}</label>
-                                <select disabled id="form-repeater-1-2" name="module" class="form-select">
+                                <label class="form-label" for="module">{{ __('lang.module') }}</label>
+                                <select disabled id="module" name="module" class="form-select">
                                     <option value="">
                                         {{ __('lang.no') }}
                                     </option>
-                                   @foreach ($modules as $name => $module)
-                                   <option value="{{ $name }}">{{$name}}</option>
-                                   @endforeach
+                                    @foreach ($modules as $name => $module)
+                                        <option value="{{ $name }}">{{ $name }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -36,73 +37,119 @@
                 </div>
             </div>
         </div>
+        <br><br><br>
         <div class="card">
             <div class="card-body">
-                <h2>{{ __('lang.new_section') }}</h2>
+                <h3 class="card-header">{{ __('lang.fields') }}</h3>
+                <div class="row">
+                    <div class="mb-3 col-lg col-md-6 mb-0">
+                        <label class="form-label" for="name">{{ __('lang.name') }}</label>
+                        <input type="text" id="name" class="form-control identifier"
+                            placeholder="{{ __('lang.name') }}" name="name" />
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label class="form-label" for="value">{{ __('lang.value') }}</label>
+                        <input type="text" id="value" class="form-control"
+                            placeholder="{{ __('lang.value') }}" name="value" />
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label class="form-label" for="type">{{ __('lang.type') }}</label>
+                        <select id="type" name="type" class="form-select">
+                            @foreach ($types as $type)
+                                <option value="{{ $type->type }}">{{ $type->type }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label style="opacity: 0">1</label>
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#spacialModal"
+                            class="btn btn-primary w-100">
+                            {{ __('lang.spacial') }}
+                        </button>
+
+                        
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label style="opacity: 0">1</label>
+                        <button 
+                        type="button" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#relationshipModal"
+                        class="btn btn-primary w-100">
+                            {{ __('lang.relationship') }}
+                        </button>
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label style="opacity: 0">1</label>
+                        <button 
+                        type="button" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#validationlModal"
+                        class="btn btn-primary w-100">
+                            {{ __('lang.validation') }}
+                        </button>
+                    </div>
+                    <div class="mb-3 col-lg mb-0 col-md-6">
+                        <label style="opacity: 0">1</label>
+                        <button type="button" class="btn btn-success w-100">{{ __('lang.add') }}</button>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
-@endsection
 
-{{-- 
-    <form action="{{ route('admin.quick-makers.store') }}" class="card QuickBuilderForm" method="POST">
-        @csrf
-        
-        <hr>
-        <h3 class="card-header">{{ __('lang.fields') }}</h3>
-        <div class="card-body">
-            <h5>{{ __('lang.database') }}</h5>
-            <div class="form-repeater">
-                <div data-repeater-list="group-a">
-                    <div data-repeater-item class="repeater-item">
+    <div class="modal fade" id="spacialModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-simple">
+            <div class="modal-content p-2">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                    <div class="py-0 rounded-top">
+                        <h2 class=" mb-4">{{ __('lang.spacial') }}</h2>
+
                         <div class="row">
-                            <div class="mb-3 col-lg col-md-6 mb-0">
-                                <label class="form-label" for="form-repeater-1-1">{{ __('lang.name') }}</label>
-                                <input type="text" id="form-repeater-1-1" class="form-control identifier"
-                                    placeholder="{{ __('lang.name') }}" name="name" />
-                            </div>
-                            <div class="mb-3 col-lg mb-0 col-md-6">
-                                <label class="form-label" for="form-repeater-1-2">{{ __('lang.value') }}</label>
-                                <input type="text" id="form-repeater-1-2" class="form-control"
-                                    placeholder="{{ __('lang.value') }}" name="value" />
-                            </div>
-                            <div class="mb-3 col-lg mb-0 col-md-6">
-                                <label class="form-label" for="form-repeater-1-3">{{ __('lang.type') }}</label>
-                                <select id="form-repeater-1-4" name="type" class="form-select">
-                                    @foreach ($types as $type)
-                                        <option value="{{ $type->type }}">{{ $type->type }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="mb-3 col-lg mb-0 col-md-6">
-                                <label class="form-label" for="form-repeater-1-7">{{ __('lang.required') }}</label>
-                                <select id="form-repeater-1-7" name="required" class="form-select">
+                            <div class="mb-3 col-lg mb-0">
+                                <label class="form-label" for="searchable">{{ __('lang.searchable') }}</label>
+                                <select id="searchable" name="searchable" class="form-select">
                                     <option value="0">{{ __('lang.no') }}</option>
                                     <option value="1">{{ __('lang.yes') }}</option>
                                 </select>
                             </div>
-                            <div class="mb-3 col-lg mb-0 col-md-6">
-                                <label class="form-label" for="form-repeater-1-8">{{ __('lang.unique') }}</label>
-                                <select id="form-repeater-1-8" name="unique" class="form-select">
+                            <div class="mb-3 col-lg mb-0">
+                                <label class="form-label" for="translatable">{{ __('lang.translatable') }}</label>
+                                <select id="translatable" name="translatable" class="form-select">
                                     <option value="0">{{ __('lang.no') }}</option>
                                     <option value="1">{{ __('lang.yes') }}</option>
                                 </select>
                             </div>
                         </div>
-                        <hr>
-                        <h5>{{ __('lang.relationship') }}</h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="relationshipModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-simple">
+            <div class="modal-content p-2">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                    <div class="py-0 rounded-top">
+                        <h2 class=" mb-4">{{ __('lang.relationship') }}</h2>
+
                         <div class="row">
                             <div class="mb-3 col-lg mb-0">
-                                <label class="form-label" for="form-repeater-1-4">{{ __('lang.relation') }}</label>
-                                <select id="form-repeater-1-4" name="relation" class="form-select relationFinder">
+                                <label class="form-label" for="relation">{{ __('lang.relation') }}</label>
+                                <select id="relation" name="relation" class="form-select relationFinder">
                                     <option value="0">{{ __('lang.no') }}</option>
                                     <option value="1">{{ __('lang.yes') }}</option>
                                 </select>
                             </div>
                             <div class="mb-3 col-lg-4 mb-0">
                                 <label class="form-label"
-                                for="form-repeater-1-5">{{ __('lang.relation_model') }}</label>
-                                <select disabled id="form-repeater-1-5" name="relation_model" class="form-select relation_model">
+                                for="relation_model">{{ __('lang.relation_model') }}</label>
+                                <select disabled id="relation_model" name="relation_model" class="form-select relation_model">
                                     @foreach ($models as $model)
                                         <option value="{{ $model }}">{{ $model }}</option>
                                     @endforeach
@@ -110,34 +157,30 @@
                             </div>
                             <div class="mb-3 col-lg-4 mb-0">
                                 <label class="form-label"
-                                    for="form-repeater-1-6">{{ __('lang.relation_key') }}</label>
-                                <input type="text" id="form-repeater-1-6" class="form-control relation_key"
+                                    for="relation_key">{{ __('lang.relation_key') }}</label>
+                                <input type="text" id="relation_key" class="form-control relation_key"
                                     placeholder="{{ __('lang.relation_key') }}" name="relation_key" disabled />
                             </div>
                         </div>
-                        <hr>
-                        <h5>{{ __('lang.spacial') }}</h5>
-                        <div class="row">
-                            <div class="mb-3 col-lg mb-0">
-                                <label class="form-label" for="form-repeater-1-9">{{ __('lang.searchable') }}</label>
-                                <select id="form-repeater-1-9" name="searchable" class="form-select">
-                                    <option value="0">{{ __('lang.no') }}</option>
-                                    <option value="1">{{ __('lang.yes') }}</option>
-                                </select>
-                            </div>
-                            <div class="mb-3 col-lg mb-0">
-                                <label class="form-label" for="form-repeater-1-10">{{ __('lang.translatable') }}</label>
-                                <select id="form-repeater-1-10" name="translatable" class="form-select">
-                                    <option value="0">{{ __('lang.no') }}</option>
-                                    <option value="1">{{ __('lang.yes') }}</option>
-                                </select>
-                            </div>
-                        </div>
-                        <h5>{{ __('lang.validation') }}</h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="validationlModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-simple">
+            <div class="modal-content p-2">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                    <div class="py-0 rounded-top">
+                        <h2 class=" mb-4">{{ __('lang.spacial') }}</h2>
+
                         <div class="row">
                             <div class="mb-3 col-lg-12 mb-0">
-                                <label class="form-label" for="form-repeater-1-11">{{ __('lang.blade_type') }}</label>
-                                <select id="form-repeater-1-11" name="blade_type" class="blade_type validationSelector form-select">
+                                <label class="form-label" for="blade_type">{{ __('lang.blade_type') }}</label>
+                                <select id="blade_type" name="blade_type" class="blade_type validationSelector form-select">
                                     <option value="" data-validation="">{{ __('lang.blade_type') }}</option>
                                     @foreach ($inputTypes as $type => $validations)
                                         <option value="{{ $type }}" data-validation="{{ json_encode($validations) }}">{{ $type }}</option>
@@ -150,42 +193,11 @@
                                    </div>
                                 </div>
                             </div>
-
-
-
-                            <div class="mb-3 col-lg d-flex align-items-center mb-0">
-                                <button class="btn w-100 btn-label-danger mt-4" type="button" data-repeater-delete>
-                                    <i class="ti ti-x ti-xs me-1"></i>
-                                    <span class="align-middle">{{ __('lang.delete') }}</span>
-                                </button>
-                            </div>
                         </div>
-                        <hr />
                     </div>
-                </div>
-                <div class="mb-0">
-                    <button class="btn btn-primary" type="button" data-repeater-create>
-                        <i class="ti ti-plus me-1"></i>
-                        <span class="align-middle">{{ __('lang.add') }} {{ __('lang.field') }}</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <div class="card-footer">
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary waves-effect waves-light SubmitQuickBuilderForm">{{ __('lang.save') }}</button>
-                <input type="hidden" name="validations" id="ValidationsData">
-            </div>
-        </div>
-    </form>
-    @push('scripts')
-        <script src="{{ asset('dashboard/vendor/libs/jquery-repeater/jquery-repeater.js') }}"></script>
-        <script src="{{ asset('dashboard/js/forms-extras.js') }}"></script>
-        <script>
 
-        </script>
-        <script src="{{ asset('dashboard/js/quick-maker/quick-maker.js') }}"></script>
-    @endpush
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
- --}}
